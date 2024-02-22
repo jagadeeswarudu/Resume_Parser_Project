6dcc02ca2ee6b8d8bcb0069a53539ea8764acc64
jagadeeswarudu <96833531+jagadeeswarudu@users.noreply.github.com>
2024-02-22 14:38:07 +0530

    Update settings.py

--- Resume_p/Resume_p/settings.py
+++ Resume_p/Resume_p/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(_file_)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
